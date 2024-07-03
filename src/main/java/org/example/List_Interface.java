1841133cb60cc88e3d2c3cc3d67f099ec3caa9f8
Hiral-Sanghani <sanghanihiral1211@gmail.com>
2024-07-03 22:41:40 +0530

    Update Programs

--- src/main/java/org/example/List_Interface.java
+++ src/main/java/org/example/List_Interface.java
@@ -1,8 +1,74 @@
 package org.example;
 
+import java.util.*;
+import java.util.function.DoubleToIntFunction;
+//import java.util.LinkedList;
+
 
 public class List_Interface {
     public static void main(String[] args) {
+
+        //String List
+        List<String> stringList = new ArrayList<>(Arrays.asList("John Herry","Doe","Olga","Amer"));
+
+        //Get Size of the list
+        System.out.println("String List Size: " + stringList.size());
+
+        //Integer List
+        LinkedList<Integer> intList1 = new LinkedList<>(Arrays.asList(90,2,14,5,4));
+        System.out.println(intList1);
+
+        //reverse list
+        Collections.reverse(intList1);
+        intList1.remove(2);
+
+        /* Remove Specific Element
+         intList1.remove(new Integer(2));   */
+
+        System.out.println(intList1);
+
+        //Convert List to Array
+        Integer[] a = intList1.toArray(new Integer[0]);
+
+        for(Integer i: a)
+            System.out.println(i);
+
+        List<Integer> intList2 = new LinkedList<>();
+        intList2.add(3);
+        intList2.add(1);
+        intList2.add(98);
+
+        System.out.println(intList2);
+
+        //get specific element from index
+        System.out.println(intList2.get(2));
+
+        System.out.println("---------");
+        // Apply Iterator
+        // Iterator<Integer> integerIterator = intList2.iterator();
+        ListIterator<Integer> integerIterator1 = intList2.listIterator();
+        while(integerIterator1.hasNext()) {
+            System.out.println(integerIterator1.next());
+        }
+
+        //Apply ListIterator - it will allow to iterate list from both side
+
+        System.out.println("----");
+
+        while(integerIterator1.hasPrevious()) {
+            System.out.println(integerIterator1.previous());
+        }
+
+        //Find Min and Max element from the list
+        System.out.println("Min from the List: " + Collections.min(intList2));
+
+        System.out.println("Max from the List: " + Collections.max(intList2));
+
+
+        /*
+        Contains method
+         */
+        System.out.println(intList1.contains(2));
 
     }
 }
--- src/main/java/org/example/Stream_Api_Basics.java
+++ src/main/java/org/example/Stream_Api_Basics.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.*;
 public class Stream_Api_Basics {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList("John","Herry","Doe"));
+        list.stream().forEach(System.out::println);
+
+        List<Integer> l2 = new ArrayList<>(Arrays.asList(1,5,3,4,5));
+        l2.stream().sorted().forEach(System.out::println);
+        System.out.println("-----");
+
+        list.stream().forEach(s -> {
+            System.out.println(s);
+        });
+
+        Stream<String> stream = list.stream().filter(s -> s.contains("John"));
+        System.out.println(stream.findFirst());
+
+        List<String> flatMapped = list.stream().flatMap(s -> Stream.of(s.split(""))).collect(Collectors.toList());
+        flatMapped.forEach(System.out::print);
+    }
 }
